16856ee3872dd9a3e14643523090c49c10e14e8e
vivek singh <viksin672@gmail.com>
2020-06-03 18:08:10 +0530

    server update

--- POC-api/POC-api/POC_api/settings.py
+++ POC-api/POC-api/POC_api/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import posixpath
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,5 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStora'
+
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+django_heroku.settings(locals())
--- POC-api/POC-api/pApi/api.py
+++ POC-api/POC-api/pApi/api.py
@@ -2,6 +2,49 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import *
+from collections import defaultdict
+from django.apps import apps
+
+
+
+#class BulkCreateManager(object):
+#    """
+#    This helper class keeps track of ORM objects to be created for multiple
+#    model classes, and automatically creates those objects with `bulk_create`
+#    when the number of objects accumulated for a given model class exceeds
+#    `chunk_size`.
+#    Upon completion of the loop that's `add()`ing objects, the developer must
+#    call `done()` to ensure the final set of objects is created for all models.
+#    """
+
+#    def __init__(self, chunk_size=100):
+#        self._create_queues = defaultdict(list)
+#        self.chunk_size = chunk_size
+
+#    def _commit(self, model_class):
+#        model_key = model_class._meta.label
+#        model_class.objects.bulk_create(self._create_queues[model_key])
+#        self._create_queues[model_key] = []
+
+#    def add(self, obj):
+#        """
+#        Add an object to the queue to be created, and call bulk_create if we
+#        have enough objs.
+#        """
+#        model_class = type(obj)
+#        model_key = model_class._meta.label
+#        self._create_queues[model_key].append(obj)
+#        if len(self._create_queues[model_key]) >= self.chunk_size:
+#            self._commit(model_class)
+
+#    def done(self):
+#        """
+#        Always call this upon completion to make sure the final partial chunk
+#        is saved.
+#        """
+#        for model_name, objs in self._create_queues.items():
+#            if len(objs) > 0:
+#                self._commit(apps.get_model(model_name))
 
 
 class Clients(APIView):
@@ -26,6 +69,7 @@ class Clients(APIView):
 			#check the request client is present in db or not
 			check = clients.objects.all().filter(client=currClient,priority=currPriority)
 			if check.count() != 0 :
+				#bulk_mgr = BulkCreateManager(chunk_size=20)
 				#loop through all clients order by priority
 				for index ,  val in enumerate(clients.objects.all().order_by('priority')):
 					#condition to exclude all lower priorities
@@ -36,14 +80,17 @@ class Clients(APIView):
 						if check2.count() == 0:
 							#increment priority of last client
 							val.priority += 1
+							#bulk_mgr.add(clients(val))
 							val.save()
 							break;
 							
 						else :
 							val.priority += 1
+							#bulk_mgr.add(clients(val))
 							val.save() 
 					else:
 						print(val.client, val.priority, '----else--',currClient,currPriority)
+			#bulk_mgr.done()
 			serializer.save()
 			return Response(serializer.data , status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
